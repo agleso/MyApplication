eb438a292f2c095a7d2717bbb7892f1362e11bd7
Oleksiy_Muzyka <oleksiy.muzyka@gmail.com>
2019-06-07 10:49:07 +0300

    Тестирую commit в git

---
+++ myapplication2/src/main/java/com/example/myapplication2/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Spinner spinner;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinner = findViewById(R.id.spinner);
+        textView = findViewById(R.id.textView);
+    }
+
+    public void showTemp(View view) {
+        int position = spinner.getSelectedItemPosition();
+        String description = getDescriptionByPosition(position);
+        textView.setText(description);
+
+    }
+    private String getDescriptionByPosition(int position) {
+        String[] description = getResources().getStringArray(R.array.explanation);
+        return description[position];
+    }
+}
